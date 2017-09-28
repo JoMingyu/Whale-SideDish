0af315039a7b85b22a51a4888bc0e8da7eab5255
PlanB <city7310@naver.com>
2017-09-29 00:02:56 +0900

    [DONE] main.py

--- Server/main.py
+++ Server/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 
-from flask import Flask, g
+from flask import Flask, request, g
 from flask_cors import CORS
 from flask_restful_swagger_2 import Api
 
@@ -8,6 +8,50 @@ app = Flask(__name__)
 CORS(app)
 
 api = Api(app, api_version='1.0')
+
+
+@app.before_first_request
+def before_first_request():
+    import logging
+    from logging.handlers import RotatingFileHandler
+
+    def initialize_logger():
+        handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
+        handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s'))
+
+        app.logger.addHandler(handler)
+        app.logger.setLevel(logging.INFO)
+
+    initialize_logger()
+    g.logger = app.logger
+    g.logger.info('------ Logger initialized ------')
+
+
+@app.before_request
+def before_request():
+    if 'logger' in g:
+        g.logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
+        g.logger.info('Request values : {0}'.format(request.values))
+
+
+@app.after_request
+def after_request(response):
+    if 'logger' in g:
+        g.logger.info('Respond : {0}'.format(response.status))
+
+    return response
+
+
+@app.teardown_request
+def teardown_request(exception):
+    if 'logger' in g and not exception:
+        g.logger.info('Teardown request successfully.')
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    if 'logger' in g and not exception:
+        g.logger.info('Teardown appcontext successfully.')
 
 
 def add_resources():
