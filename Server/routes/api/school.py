91fdec7641d574d3c57508a0a9fe36474b211ab8
PlanB <city7310@naver.com>
2017-09-29 00:02:52 +0900

    [Add] Swagger on school API

--- Server/routes/api/school.py
+++ Server/routes/api/school.py
@@ -1,11 +1,67 @@
 # -*- coding: utf8 -*-
 
-from flask_restful import Resource, request
+from flask_restful_swagger_2 import swagger, Resource, request
 
 from db.models.school import SchoolModel
 
 
 class School(Resource):
+    @swagger.doc({
+        'tags': ['학교'],
+        'description': '학교 이름 검색',
+
+        'parameters': [
+            {
+                'name': 'key',
+                'description': '사용자의 검색 키워드',
+                'in': 'query',
+                'type': 'String'
+            }
+        ],
+
+        'responses': {
+            '200': {
+                'description': '검색 성공(데이터 있음)',
+                'examples': {
+                    'application/json': [
+                        {
+                            "name": "대덕여자고등학교",
+                            "code": "C100000376",
+                            "region": "부산광역시"
+                        },
+                        {
+                            "name": "대덕고등학교",
+                            "code": "G100000167",
+                            "region": "대전광역시"
+                        },
+                        {
+                            "name": "대덕공업고등학교",
+                            "code": "G100000168",
+                            "region": "대전광역시"
+                        },
+                        {
+                            "name": "대덕여자고등학교",
+                            "code": "G100000169",
+                            "region": "대전광역시"
+                        },
+                        {
+                            "name": "대덕소프트웨어마이스터고등학교",
+                            "code": "G100000170",
+                            "region": "대전광역시"
+                        },
+                        {
+                            "name": "대덕종합고등학교",
+                            "code": "Q100000193",
+                            "region": "전라남도"
+                        }
+                    ]
+                }
+            },
+            '204': {
+                'description': '검색 실패(데이터 없음)'
+            }
+        }
+    })
     def get(self):
         key = request.args.get('key')
         searched_school_data = []
