b30c742aa0df1e6991bbf4a4ced5652478e14c0e
AshtonYoon <y109384@naver.com>
2017-10-06 06:09:35 +0900

    add main component (final)

--- whale-sidedish/src/app/app.module.ts
+++ whale-sidedish/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InitialComponent } from './initial/initial.component';
 import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InitialComponent,
     SearchComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule
