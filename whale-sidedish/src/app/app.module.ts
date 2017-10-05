00e575a995f5e363ca372dfba413b1c857438202
AshtonYoon <y109384@naver.com>
2017-10-06 05:56:38 +0900

    redesign search component

--- whale-sidedish/src/app/app.module.ts
+++ whale-sidedish/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InitialComponent } from './initial/initial.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InitialComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule
