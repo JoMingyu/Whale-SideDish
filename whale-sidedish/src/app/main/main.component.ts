3272fc311dbeb428f76793b4a777b4cd6a8557ee
AshtonYoon <y109384@naver.com>
2017-10-06 23:13:42 +0900

    design main component

--- whale-sidedish/src/app/main/main.component.ts
+++ whale-sidedish/src/app/main/main.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  public meals = [
+    {
+      "time" : "아침",
+      "content" : "시래기된장국, 돼지고기산적, 매운맛당면김말이, 케찹, 석박지, 도시락김, 보리밥"
+    },
+    {
+      "time" : "아침",
+      "content" : "시래기된장국, 돼지고기산적, 매운맛당면김말이, 케찹, 석박지, 도시락김, 보리밥"
+    },
+    {
+      "time" : "아침",
+      "content" : "시래기된장국, 돼지고기산적, 매운맛당면김말이, 케찹, 석박지, 도시락김, 보리밥"
+    }];
 
   constructor() { }
 
