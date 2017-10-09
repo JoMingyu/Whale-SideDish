11bb8b1e3b6ba6cd373dcadd7b65a35e872f9dd3
AshtonYoon <y109384@naver.com>
2017-10-09 13:18:47 +0900

    binding current meal

--- whale-sidedish/src/app/main/main.component.ts
+++ whale-sidedish/src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { SearchComponent } from '../search/search.component';
 import { HttpServiceService } from '../services/http-service.service';
+import { Meal } from '../model/meal.model';
 
 @Component({
   selector: 'app-main',
@@ -10,32 +11,12 @@ import { HttpServiceService } from '../services/http-service.service';
   providers: [SearchComponent]
 })
 export class MainComponent implements OnInit {
-  public meals = [
-    {
-      "time" : "아침",
-      "content" : "시래기된장국, 돼지고기산적, 매운맛당면김말이, 케찹, 석박지, 도시락김, 보리밥"
-    },
-    {
-      "time" : "아침",
-      "content" : "시래기된장국, 돼지고기산적, 매운맛당면김말이, 케찹, 석박지, 도시락김, 보리밥"
-    },
-    {
-      "time" : "아침",
-      "content" : "시래기된장국, 돼지고기산적, 매운맛당면김말이, 케찹, 석박지, 도시락김, 보리밥"
-    }];
+  public meals : Object[] = Meal.singleton().meals;
 
-  constructor(public school: SearchComponent, private httpService: HttpServiceService) { 
-    
+  constructor(public school: SearchComponent, 
+    private httpService: HttpServiceService) {
   }
 
   ngOnInit() {
-  }
-
-  showMeal(){
-    var data = this.httpService.getMeals(
-      this.school.currentSchool.code, 
-      new Date().toISOString().slice(0,10)); 
-
-    console.log(data);
   }
 }
--- whale-sidedish/src/app/search/search.component.ts
+++ whale-sidedish/src/app/search/search.component.ts
@@ -3,8 +3,10 @@ import { Http, Headers } from '@angular/http'
 import { InitialComponent } from '../initial/initial.component';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { HttpServiceService } from '../services/http-service.service';
 import { MainComponent } from '../main/main.component';
+import { Meal } from '../model/meal.model';
 
 @Component({
   selector: 'app-search',
@@ -47,7 +49,6 @@ export class SearchComponent implements OnInit {
   selectSchool(school: any){
     let index = this.schools.indexOf(school);
 
-    //아 ㅜㅜㅜ 이러고 싶지 않은데 :(
     let svgs = document.getElementsByClassName('check');
 
     for(let i = 0; i < svgs.length; i++)
@@ -61,23 +62,35 @@ export class SearchComponent implements OnInit {
     this.initialInstance.NavigationStackCount++;
 
     //new Date().toISOString().slice(0,10)
-    var data = this.httpService.getMeals(
-      this.currentSchool.code, 
-      "2017-10-11")
-      .subscribe(
-        data => {
-          if(data.status == 205){
-            console.log("서버에 정보가 없습니다")
-          }
-          else if(data.status == 204){
-           console.log('parse error');
-          }
-          else{
-            console.log(data.json());
-          }
-        },
-        error => console.log('parse error'),
-        () => {}
-      );
+    this.httpService.getMeals(
+      this.currentSchool.code, "2017-10-11")
+    .subscribe(
+      data => {
+        if(data.status == 205){
+          console.log("서버에 정보가 없습니다")
+        }
+        else if(data.status == 204){
+          console.log('parse error');
+        }
+        else{
+          let pattern = /\[|\]|\'/g;
+
+          let breakfast = (data.json().breakfast as string).replace(pattern, '');
+          let lunch = (data.json().lunch as string).replace(pattern, '');
+          let dinner = (data.json().dinner as string).replace(pattern, '');
+
+          Meal.singleton().meals.push(
+            breakfast);
+
+          Meal.singleton().meals.push(
+            lunch);
+
+          Meal.singleton().meals.push(
+            dinner);
+        }
+      },
+      error => console.log('parse error'),
+      () => {}
+    );
   }
 }
